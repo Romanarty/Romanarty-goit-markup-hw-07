b25bd0f66bf404410490921131cc899dc2deeaea
Roman <romanarty@gmail.com>
2021-03-23 13:34:06 +0200

    23 03

---
+++ js/mobile-menu.js
@@ -0,0 +1,20 @@
+(() => {
+  const mobileMenu = document.querySelector("[mobile-modal]");
+  const openMenuBtn = document.querySelector("[mobile-modal-open]");
+  const closeMenuBtn = document.querySelector("[mobile-modal-close]");
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+  };
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+  });
+})();
